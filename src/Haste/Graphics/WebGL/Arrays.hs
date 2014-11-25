cf26106cbaee8ba7164f365b0d5b0ba9af663410
Matthew Spellings <mspells@umich.edu>
2014-11-25 16:02:13 -0500

    Expanded TypedArray API

--- src/Haste/Graphics/WebGL/Arrays.hs
+++ src/Haste/Graphics/WebGL/Arrays.hs
@@ -2,6 +2,7 @@
 
 module Haste.Graphics.WebGL.Arrays where
 
+import Control.Monad (liftM2)
 import Haste.Foreign
 import Haste.Prim
 import Foreign.Ptr
@@ -11,22 +12,32 @@ newtype ArrayBuffer = ArrayBuffer JSAny deriving (Pack, Unpack)
 newArrayBuffer::Int->IO ArrayBuffer
 newArrayBuffer = ffi "(function(byteLength) {return ArrayBuffer(byteLength);})"
 
+arrayBufferLength::ArrayBuffer->IO Int
+arrayBufferLength = ffi "(function(buf) {return buf.length;})"
+
 class (Pack a, Unpack a)=>TypedArray a where
   type EltType a
   newSizedArray::Int->IO a
   copyTypedArray::TypedArray b=>b->IO a
   fromJSArray::Unpack (EltType a)=>[EltType a]->IO a
-  fromArrayBuffer::Int->Int->ArrayBuffer->IO a
+  fromArrayBufferSized::Int->Int->ArrayBuffer->IO a
+  fromArrayBuffer::ArrayBuffer->IO a
 
   byteLength::a->IO Int
   byteLength = ffi "(function(arr) {return arr.byteLength;})"
   bytesPerElement::a->IO Int
   bytesPerElement = ffi "(function(arr) {return arr.BYTES_PER_ELEMENT;})"
+  arrayLength::a->IO Int
+  arrayLength arr = liftM2 quot (byteLength arr) (bytesPerElement arr)
 
   getIndex::Pack (EltType a)=>a->Int->IO (EltType a)
   getIndex = ffi "(function(arr, idx) {return arr[idx];})"
   setIndex::Unpack (EltType a)=>a->Int->EltType a->IO ()
   setIndex = ffi "(function(arr, idx, val) {arr[idx] = val;})"
+  toList::Pack (EltType a)=>a->IO [EltType a]
+  toList arr = do
+    length <- arrayLength arr
+    mapM (getIndex arr) [0..length - 1]
 
 newtype Int8Array = Int8Array JSAny deriving (Pack, Unpack)
 
@@ -35,7 +46,8 @@ instance TypedArray Int8Array where
   newSizedArray = ffi "(function(size) {return new Int8Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Int8Array(other);})"
   fromJSArray = ffi "(function(other) {return new Int8Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Int8Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Int8Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Int8Array(buffer);})"
 
 newtype Int16Array = Int16Array JSAny deriving (Pack, Unpack)
 
@@ -44,7 +56,8 @@ instance TypedArray Int16Array where
   newSizedArray = ffi "(function(size) {return new Int16Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Int16Array(other);})"
   fromJSArray = ffi "(function(other) {return new Int16Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Int16Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Int16Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Int16Array(buffer);})"
 
 newtype Int32Array = Int32Array JSAny deriving (Pack, Unpack)
 
@@ -53,7 +66,8 @@ instance TypedArray Int32Array where
   newSizedArray = ffi "(function(size) {return new Int32Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Int32Array(other);})"
   fromJSArray = ffi "(function(other) {return new Int32Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Int32Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Int32Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Int32Array(buffer);})"
 
 newtype Int64Array = Int64Array JSAny deriving (Pack, Unpack)
 
@@ -62,7 +76,8 @@ instance TypedArray Int64Array where
   newSizedArray = ffi "(function(size) {return new Int64Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Int64Array(other);})"
   fromJSArray = ffi "(function(other) {return new Int64Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Int64Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Int64Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Int64Array(buffer);})"
 
 newtype Uint8Array = Uint8Array JSAny deriving (Pack, Unpack)
 
@@ -71,7 +86,8 @@ instance TypedArray Uint8Array where
   newSizedArray = ffi "(function(size) {return new Uint8Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Uint8Array(other);})"
   fromJSArray = ffi "(function(other) {return new Uint8Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Uint8Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Uint8Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Uint8Array(buffer);})"
 
 newtype Uint16Array = Uint16Array JSAny deriving (Pack, Unpack)
 
@@ -80,7 +96,8 @@ instance TypedArray Uint16Array where
   newSizedArray = ffi "(function(size) {return new Uint16Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Uint16Array(other);})"
   fromJSArray = ffi "(function(other) {return new Uint16Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Uint16Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Uint16Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Uint16Array(buffer);})"
 
 newtype Uint32Array = Uint32Array JSAny deriving (Pack, Unpack)
 
@@ -89,7 +106,8 @@ instance TypedArray Uint32Array where
   newSizedArray = ffi "(function(size) {return new Uint32Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Uint32Array(other);})"
   fromJSArray = ffi "(function(other) {return new Uint32Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Uint32Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Uint32Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Uint32Array(buffer);})"
 
 newtype Float32Array = Float32Array JSAny deriving (Pack, Unpack)
 
@@ -98,4 +116,5 @@ instance TypedArray Float32Array where
   newSizedArray = ffi "(function(size) {return new Float32Array(size);})"
   copyTypedArray = ffi "(function(other) {return new Float32Array(other);})"
   fromJSArray = ffi "(function(other) {return new Float32Array(other);})"
-  fromArrayBuffer = ffi "(function(byteOffset, length, buffer) {return new Float32Array(buffer, byteOffset, length);})"
+  fromArrayBufferSized = ffi "(function(byteOffset, length, buffer) {return new Float32Array(buffer, byteOffset, length);})"
+  fromArrayBuffer = ffi "(function(buffer) {return new Float32Array(buffer);})"
